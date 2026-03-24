61dc9daec9f22aa0b92b5c111e116a295023e9bf
unknown <viczik16@gmail.com>
2026-03-24 19:25:19 +0100

    added hosts

--- Docai_api/settings.py
+++ Docai_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-3c$#6baxupd5tc^drc!04xwa5qrc!$wcm@^t2q9_wfi+wjxy!$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://docai-api-if8x.onrender.com", "localhost", "127.0.0.1"]
 
 # Application definition
 INSTALLED_APPS = [
